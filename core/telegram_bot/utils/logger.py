82b74dc7ea714d6dce37f33d7b9534a9c27ca7a6
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-26 01:10:49 +0300

    Создание логгера

---
+++ core/telegram_bot/utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(filename)s:%(lineno)d #%(levelname)-8s "
+    "[%(asctime)s] - %(name)s - %(message)s",
+)
+
+
+# config.logger.path_log
+file_handler = logging.FileHandler(
+    filename='info.log', 
+    encoding='utf-8')
+
+file_handler.setFormatter(
+    logging.Formatter("%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s")
+)
+
+logging.getLogger().addHandler(file_handler)
