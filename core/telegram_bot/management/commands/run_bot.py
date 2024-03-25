3db6c34e7cc423203642fb76008d1df885649090
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-25 23:37:53 +0300

    Структура запуска бота через команду
    Команда: python manage.py run_server

---
+++ core/telegram_bot/management/commands/run_bot.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from typing import Any
+
+
+
+class Command(BaseCommand):    
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        ...
