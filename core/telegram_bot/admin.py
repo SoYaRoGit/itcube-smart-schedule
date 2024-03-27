c96c863ad9abcb36c1a418a5b9f161d4ae05de50
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-27 21:25:16 +0300

    Регистрация модели (доп. данные о ученике)

--- core/telegram_bot/admin.py
+++ core/telegram_bot/admin.py
@@ -5,5 +5,36 @@ from telegram_bot import models
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
     '''Регистрация модуля для учеников'''
-    list_display = ('id', 'full_name', 'login', 'password', 'telegram_id', 'if_fourteen')
+    list_display = ('id', 
+                    'full_name', 
+                    'login', 
+                    'password', 
+                    'telegram_id'
+    )
+    
     list_display_links = ['full_name']
+    
+    
+@admin.register(models.StudentContentDetails)
+class StudentContentDetailsAdmin(admin.ModelAdmin):
+    '''Регистрация модуля для персональных данных ученика'''
+
+    list_display = ('id', 
+                    'student', 
+                    'parent_full_name', 
+                    'parent_residential_adress', 
+                    'date_birth', 
+                    'if_fourteen', 
+                    'student_residential_adress',
+                    'passport_data', 
+                    'passport_data_issued_by', 
+                    'passport_data_date_of_issueс',
+                    'name_education_organization', 
+                    'certificate_number',
+                    'parent_contact', 
+                    'student_contact', 
+                    'medical_restrictions', 
+                    'date_contract'
+    )
+    
+    list_display_links = ['student']
--- core/telegram_bot/models.py
+++ core/telegram_bot/models.py
@@ -20,20 +20,12 @@ class Student(models.Model):
     
     full_name = models.CharField(
         verbose_name = 'ФИО',
-        max_length = 45,
+        max_length = 50,
         unique = False,
         blank = False,
         help_text = 'Укажите ФИО ученика'
     )
     
-    if_fourteen = models.BooleanField(
-        verbose_name = 'Достиг ли ученик 14-го возраста',
-        blank = False,
-        default = False,
-        help_text = 'Укажите достиг ли ученик 14-го возраста'
-        
-    )
-
     telegram_id = models.PositiveBigIntegerField(
         verbose_name = 'Телеграм ID', 
         blank = True, 
@@ -49,3 +41,116 @@ class Student(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class StudentContentDetails(models.Model):    
+    parent_full_name = models.CharField(
+        verbose_name = 'ФИО Родителя',
+        max_length = 50,
+        blank = True,
+        null = True,
+        help_text = 'Укажите ФИО Родителя (законного представителя)'
+    )
+    
+    parent_residential_adress = models.CharField(
+        verbose_name = 'Место регистрации родителя',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите место регистрации родителя (законного представителя)',
+    )
+
+    student = models.OneToOneField(
+        to = Student,
+        on_delete = models.CASCADE,
+        verbose_name = 'Ученик',
+        blank = False,
+        help_text = 'Укажите для кого указываются данные'
+    )
+    
+    date_birth = models.DateField(
+        verbose_name = 'Дата рождения',
+        blank = True,
+        help_text = "Укажите дату рождения"
+    )
+    
+    if_fourteen = models.BooleanField(
+        verbose_name = 'Достиг ли ученик 14-го возраста',
+        default = False,
+        help_text = 'Укажите достиг ли ученик 14-го возраста'
+    )
+    
+    student_residential_adress = models.CharField(
+        verbose_name = 'Адрес проживания',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите адрес проживания с индексом',
+    )
+    
+    passport_data = models.CharField(
+        verbose_name = 'Паспортные данные',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите паспортные данные(номер, серия)'
+    )
+    
+    passport_data_issued_by = models.CharField(
+        verbose_name = 'Кем выдан',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите кем выдан паспорт'
+    )
+    
+    passport_data_date_of_issueс = models.DateField(
+        verbose_name = 'Дата выдачи',
+        blank = True,
+        help_text = 'Укажите дату выдачи паспорта'
+    )
+    
+    name_education_organization = models.CharField(
+        verbose_name = 'Учебная организация',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите название учебной организации'
+    )
+    
+    certificate_number = models.CharField(
+        verbose_name = 'Номер сертификата',
+        max_length = 10,
+        unique = True,
+        blank = False,
+        help_text = 'Укажите номер сертификата'
+    )
+    
+    parent_contact = models.CharField(
+        verbose_name = 'Контактные данные родителя',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите контактные данные родителя (номер телефона, эл. почта)'
+    )
+    
+    student_contact = models.CharField(
+        verbose_name = 'Контактные данные ученика',
+        max_length = 255,
+        blank = True,
+        help_text = 'Укажите контактные данные родителя (номер телефона, эл. почта)'
+    )
+    
+    medical_restrictions = models.BooleanField(
+        verbose_name = 'Медицинские ограничения',
+        default = False,
+        help_text = 'Укажите медицинские ограничения (да/нет)'
+    )
+    
+    date_contract = models.DateField(
+        verbose_name = 'Дата заключения контракта',
+        blank = False,
+        help_text = 'Укажите дату заключения контракта'
+    )    
+    
+    class Meta:
+        verbose_name = "Дополнительная информация о ученике"
+        verbose_name_plural = "Дополнительная информация о учениках"
+        
+    
+    def __str__(self) -> str:
+        return self.student.full_name
