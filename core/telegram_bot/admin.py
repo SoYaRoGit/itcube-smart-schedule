63b9f98afdd80fd5b3b11a27228ca54e37dc094b
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-27 09:39:05 +0300

    Регистрация модели в админке (Ученики)

--- core/telegram_bot/admin.py
+++ core/telegram_bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from telegram_bot import models
 
-# Register your models here.
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+    '''Регистрация модуля для учеников'''
+
+    list_display = ('id', 'full_name', 'login', 'password', 'telegram_id', 'if_fourteen')
