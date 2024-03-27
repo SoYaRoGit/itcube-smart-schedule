aa98e87aa1cd69a21552bcbf9719a79ab6345dcf
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-27 09:33:54 +0300

    Реализация поле Student ver. 1.0

--- core/telegram_bot/models.py
+++ core/telegram_bot/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    "Ученики"
+    telegram_id = models.PositiveBigIntegerField(
+        verbose_name = 'Телеграм ID', 
+        blank = True, 
+        null = True, 
+        unique = True, 
+        db_index = True, 
+        help_text = 'Указывается телеграм ID после аутентификации'
+    )
+    
+    login = models.CharField(
+        verbose_name = 'Логин в базе данных',
+        max_length = 30,
+        unique = True,
+        blank = False,
+        help_text = 'Укажите логин для ученика'
+    )
+    
+    password = models.CharField(
+        verbose_name = 'Пароль в базе данных',
+        max_length = 30,
+        blank = False,
+        help_text = 'Укажите пароль для ученика'
+    )
+    
+    full_name = models.CharField(
+        verbose_name = 'ФИО',
+        max_length = 45,
+        unique = False,
+        blank = False,
+        help_text = 'Укажите ФИО ученика'
+    )
+    
+    if_fourteen = models.BooleanField(
+        verbose_name = 'Достиг ли ученик 14-го возраста',
+        blank = False,
+        default = False,
+        help_text = 'Укажите достиг ли ученик 14-го возраста'
+        
+    )
+    
+    class Meta:
+        verbose_name = "Ученик"
+        verbose_name_plural = "Ученики"
+
+    def __str__(self):
+        return self.full_name
