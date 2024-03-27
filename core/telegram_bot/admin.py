930e52b0525e309453de4c93cb6887f3e60557f7
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-27 09:47:00 +0300

    Сделал поле "ФИО" кликабельным

--- core/telegram_bot/admin.py
+++ core/telegram_bot/admin.py
@@ -5,5 +5,5 @@ from telegram_bot import models
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
     '''Регистрация модуля для учеников'''
-
     list_display = ('id', 'full_name', 'login', 'password', 'telegram_id', 'if_fourteen')
+    list_display_links = ['full_name']
