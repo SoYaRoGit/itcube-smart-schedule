45a9e59fb02b6fc603a9ccce5ee16216b6b15f79
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-30 09:08:51 +0300

    Вынес неправильные обработчики в нужный роутер

--- core/telegram_bot/states/authentication_state.py
+++ core/telegram_bot/states/authentication_state.py
@@ -5,31 +5,12 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, StatesGroup, State
 from telegram_bot.lexicon.authentication import AUTHENTICATION_TEXT
 from telegram_bot.keyboards.authentication_keyboard import (
-    inline_keyboard_authentication,
-    inline_keyboard_authentication_user_menu,
     inline_keyboard_authentication_cancel
 )
 
 
+
 authentication_state_router = Router()
-
-
-@authentication_state_router.callback_query(F.data.in_('authentication_user'))
-async def authentication_user_menu(callback: CallbackQuery):
-    await callback.message.edit_text(
-        text = AUTHENTICATION_TEXT['authentication_user_menu'],
-        reply_markup = inline_keyboard_authentication_user_menu
-    )
-    
-    await callback.answer()
-
-
-@authentication_state_router.callback_query(F.data.in_('authentication_user_backward'))
-async def authentication_user_backward(callback: CallbackQuery):
-    await callback.message.edit_text(
-        text = AUTHENTICATION_TEXT['cmd_start_not_authentication'],
-        reply_markup = inline_keyboard_authentication
-    )
 
 
 class FSMAuthenticationForm(StatesGroup):
