3b4392fcc78122a3a47a8c35004847d79e54c531
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-30 08:28:26 +0300

    Решил вернуть прошлый формат фильтра
    т.к. он работает 0.0

--- core/telegram_bot/filters/filter.py
+++ core/telegram_bot/filters/filter.py
@@ -5,27 +5,27 @@ from asgiref.sync import sync_to_async
 
 
 
+def is_authenticated(telegram_id: int) -> bool:
+    # Проверяем, существует ли студент с указанным telegram_id
+    student_exists = Student.objects.filter(telegram_id=telegram_id).exists()
+        
+    # Если студент существует, проверяем его аутентификацию
+    if student_exists:
+        student = Student.objects.get(telegram_id=telegram_id)
+        if student.is_authentication:
+            return False  # Если аутентифицирован, возвращаем False
+        
+    # Если студент не найден или не аутентифицирован, проверяем преподавателя
+    teacher_exists = Teacher.objects.filter(telegram_id=telegram_id).exists()
+    if teacher_exists:
+        teacher = Teacher.objects.get(telegram_id=telegram_id)
+        if teacher.is_authentication:
+            return False  # Если аутентифицирован, возвращаем False
+        
+    # Если ни студент, ни преподаватель не найдены или не аутентифицированы, возвращаем True
+    return True
+
 class NotAuthenticationFilter(BaseFilter):
-    def is_authenticated(telegram_id: int) -> bool:
-        # Проверяем, существует ли студент с указанным telegram_id
-        student_exists = Student.objects.filter(telegram_id=telegram_id).exists()
-            
-        # Если студент существует, проверяем его аутентификацию
-        if student_exists:
-            student = Student.objects.get(telegram_id=telegram_id)
-            if student.is_authentication:
-                return False  # Если аутентифицирован, возвращаем False
-            
-        # Если студент не найден или не аутентифицирован, проверяем преподавателя
-        teacher_exists = Teacher.objects.filter(telegram_id=telegram_id).exists()
-        if teacher_exists:
-            teacher = Teacher.objects.get(telegram_id=telegram_id)
-            if teacher.is_authentication:
-                return False  # Если аутентифицирован, возвращаем False
-            
-        # Если ни студент, ни преподаватель не найдены или не аутентифицированы, возвращаем True
-        return True
-    
     async def __call__(self, message: Message) -> bool:
         telegram_id = message.from_user.id
-        return await sync_to_async(self.is_authenticated)(telegram_id)
+        return await sync_to_async(is_authenticated)(telegram_id)
