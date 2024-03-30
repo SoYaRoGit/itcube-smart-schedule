a633afadf36394f0d5edb94bbc06aa1607aef821
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-03-30 08:48:41 +0300

    Реализация инлайн кнопки отмены в момент авторизации

--- core/telegram_bot/states/authentication_state.py
+++ core/telegram_bot/states/authentication_state.py
@@ -3,8 +3,11 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, StatesGroup, State
-from telegram_bot.keyboards.authentication_keyboard import inline_keyboard_authentication_user_menu
 from telegram_bot.lexicon.authentication import AUTHENTICATION_TEXT
+from telegram_bot.keyboards.authentication_keyboard import (
+    inline_keyboard_authentication,
+    inline_keyboard_authentication_user_menu
+)
 
 
 authentication_state_router = Router()
@@ -18,3 +21,11 @@ async def authentication_user_menu(callback: CallbackQuery):
     )
     
     await callback.answer()
+
+
+@authentication_state_router.callback_query(F.data.in_('authentication_user_cancel'))
+async def authentication_user_cancel(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text = AUTHENTICATION_TEXT['cmd_start_not_authentication'],
+        reply_markup = inline_keyboard_authentication
+    )
