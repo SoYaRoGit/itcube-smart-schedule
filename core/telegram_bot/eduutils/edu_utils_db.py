96057b5b2a4cf950973f4a12a29b280ed01ae16d
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-04-06 14:14:16 +0300

    Тестовая реализация вывода в консоль оповещения

--- core/telegram_bot/eduutils/edu_utils_db.py
+++ core/telegram_bot/eduutils/edu_utils_db.py
@@ -1,7 +1,8 @@
+from django.utils import timezone
 from datetime import datetime
 from django.db.models import Q
 from telegram_bot.models import Student, Schedule
-
+from datetime import timedelta
 
 
 def __student_to_dict(student):
@@ -40,3 +41,32 @@ def get_student_send_schedule(telegram_id: int):
     schedule_strings = [str(schedule) for schedule in schedules]
     
     return schedule_strings
+
+
+def send_schedule_reminder():
+    now = datetime.now()  # Получаем текущую дату и время
+    notification_time = now + timedelta(minutes=1) # Время оповещения 
+    
+    students = Student.objects.all()
+    
+    students_schedule = {}
+    
+    for student in students:
+        schedules = Schedule.objects.filter(
+            Q(date=now.date(), start_time__gte=now.time()) | Q(date__gt=now.date()), # Начинающиеся после текущего времени
+            group__students=student,
+            date__gte=now.date(),  # Занятия начиная с сегодняшнего дня
+        ).order_by('date', 'start_time').all()
+        
+        schedule_strings = []
+        
+        for schedule in schedules:
+            start_datetime = timezone.datetime.combine(schedule.date, schedule.start_time)
+            # Проверяем, находится ли текущее время за 30 минут до начала занятия
+            if notification_time >= start_datetime >= now:
+                # Если условие выполняется, добавляем информацию о занятии в список для оповещения
+                schedule_strings.append(f"{schedule.subject} в {schedule.start_time}")
+        
+        students_schedule[student.telegram_id] = schedule_strings
+    
+    return students_schedule
