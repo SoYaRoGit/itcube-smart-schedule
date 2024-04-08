59d4a06251ba66a09c077a402ff5089d1f3945cc
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-04-08 21:53:00 +0300

    Реализация команды /panel для преподавателя(без функционала)

--- core/telegram_bot/handlers/teacher_handler.py
+++ core/telegram_bot/handlers/teacher_handler.py
@@ -1,6 +1,19 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
+from telegram_bot.filters.filter import AuthenticationTeacherFilter
+from telegram_bot.keyboards.teacher_keyboard import inline_keyboard_panel
 
 
 
 teacher_handler = Router()
+
+teacher_handler.message(AuthenticationTeacherFilter())
+
+
+@teacher_handler.message(F.text == '/panel')
+async def cmd_panel_teacher(message: Message):
+    await message.delete()
+    await message.answer(
+        text = 'Панель преподавателя',
+        reply_markup = inline_keyboard_panel
+    )
