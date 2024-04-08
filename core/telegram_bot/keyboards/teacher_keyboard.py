a9b38ed5682ac7332409551dd1b96b9c458b64b0
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-04-08 21:52:45 +0300

    Создал панель для преподавателя

---
+++ core/telegram_bot/keyboards/teacher_keyboard.py
@@ -0,0 +1,25 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+inline_keyboard_panel = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text='Личные данные',
+                callback_data='teacher_send_personal_data'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='Расписание',
+                callback_data='teacher_send_schedule'
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text='Отмена',
+                callback_data='teacher_panel_cancel'
+            )
+        ]
+    ]
+)
