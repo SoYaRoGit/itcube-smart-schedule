671d3677b481844a31f89034b87649332fc4a9a6
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-04-17 12:02:47 +0300

    Документация для apps.py

--- core/telegram_bot/apps.py
+++ core/telegram_bot/apps.py
@@ -1,7 +1,15 @@
 from django.apps import AppConfig
 
-
 class TelegramBotConfig(AppConfig):
+    """
+    Конфигурация приложения телеграм-бота.
+
+    Attributes:
+        default_auto_field (str): Имя поля для автоматического установки первичного ключа.
+        name (str): Имя приложения.
+        verbose_name (str): Отображаемое имя приложения.
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "telegram_bot"
     verbose_name = "Телеграм Бот"
