cc45c632dacbdeca8c032c4b65b405a6fa1d6452
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-04-17 12:00:13 +0300

    Документация к loader.py

--- core/telegram_bot/loader.py
+++ core/telegram_bot/loader.py
@@ -3,6 +3,11 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from django.conf import settings
 
+# Создание асинхронного планировщика задач
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+
+# Создание объекта телеграм-бота
 bot = Bot(settings.TOKEN_TELEGRAM_BOT, parse_mode="HTML")
+
+# Создание диспетчера для обработки входящих сообщений бота
 dp = Dispatcher(storage=MemoryStorage())
